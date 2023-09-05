84303533ba826b85a279357e542b3975736655ac
Vaibhavi Rastogi <144164091+vaibhavirastogi@users.noreply.github.com>
2023-09-05 13:15:31 -0500

    Create apb_sequencer.sv

---
+++ apb_sequencer.sv
@@ -0,0 +1,10 @@
+class apb_sequencer extends uvm_sequencer#(apb_seq_item);
+  
+  `uvm_component_utils(apb_sequencer)
+  
+  function new(string name="apb_sequencer", uvm_component parent=null);
+    super.new(name, parent);
+  endfunction
+  
+
+endclass
